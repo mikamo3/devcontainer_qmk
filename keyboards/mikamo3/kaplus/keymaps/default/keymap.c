8f6be953a256afefde4d22afcc85fbf20af5742f
mikamo3 <kamo3proj@gmail.com>
2022-12-26 23:20:17 +0900

    :sparkles: (kaplus) fix keymap

--- keyboards/mikamo3/kaplus/keymaps/default/keymap.c
+++ keyboards/mikamo3/kaplus/keymaps/default/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T,KC_MINS,KC_EQL, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
         KC_TAB,KC_A, KC_S, KC_D, KC_F, KC_G,KC_LBRC,KC_RBRC, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_BSLS,
     KC_LCTL,  KC_Z, KC_X, KC_C, KC_V, KC_B,               KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
-            KC_LSFT,C(KC_SPC),TD(TD_LGUI),KC_LOWER,LCTL_T(KC_ESC),LSFT_T(KC_BSPC),KC_RAISE,TD(TD_RGUI),KC_RALT,TD(TD_RSFTJP)
+            KC_LSFT,KC_LALT,TD(TD_LGUI),KC_LOWER,LCTL_T(KC_ESC),LSFT_T(KC_BSPC),KC_RAISE,TD(TD_RGUI),KC_RALT,TD(TD_RSFTJP)
 
     ),
     [_LOWER] = LAYOUT(
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_RAISE] = LAYOUT(
         _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,KC_F11,KC_F12,KC_F6,KC_F7, KC_F8, KC_F9, KC_F10,  _______,
-_______,_______,KC_END,KC_PGDN,KC_PGUP,KC_HOME,_______,_______,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,_______,_______,
+_______,KC_HOME,KC_PGDN,KC_PGUP,KC_END,_______,_______,_______,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,_______,_______,
 _______,_______,KC_PSCR,KC_INS,KC_SLCK,KC_PAUS,_______,_______,_______,_______,_______,_______,
     _______,_______,_______,_______,_______,LSFT_T(KC_DEL),_______,_______,_______,_______
 
@@ -73,7 +73,7 @@ _______,_______,KC_PSCR,KC_INS,KC_SLCK,KC_PAUS,_______,_______,_______,_______,_
     ),
     [_RAISE_JP] = LAYOUT(
         _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,KC_F11,KC_F12,KC_F6,KC_F7, KC_F8, KC_F9, KC_F10,  _______,
-_______,_______,KC_END,KC_PGDN,KC_PGUP,KC_HOME,_______,_______,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,_______,_______,
+_______,KC_HOME,KC_PGDN,KC_PGUP,KC_END,_______,_______,_______,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,_______,_______,
 _______,_______,KC_PSCR,KC_INS,KC_SLCK,KC_PAUS,_______,_______,_______,_______,_______,_______,
     _______,_______,_______,_______,_______,LSFT_T(KC_DEL),_______,_______,_______,_______
 
