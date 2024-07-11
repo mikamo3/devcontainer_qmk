5b40c78e48fa19b1368b93a3a994fd31603501c4
mikamo3 <kamo3proj@gmail.com>
2024-07-12 00:07:24 +0900

    fix laplus

--- keyboards/mikamo3/laplus/keymaps/default/keymap.c
+++ keyboards/mikamo3/laplus/keymaps/default/keymap.c
@@ -9,8 +9,8 @@ enum layer_names {
 
 enum custom_keycodes {
     BASE = SAFE_RANGE,
-    LOWER,
-    RAISE,
+    KC_LOWER,
+    KC_RAISE,
 };
 
 #define SPC_LWR LT(_LOWER,KC_SPC)
@@ -29,10 +29,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ESC_CTL, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_MINS,       KC_EQL,  KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,    BSP_CTL,
         TAB_SFT, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_LBRC,       KC_RBRC, KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, BSH_SFT,
                  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   GUI_H,         GUI_L,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,
-                                 KC_LALT,KC_LGUI,SPC_LWR,ESC_CTL,       BSP_SFT, ENT_RSE,KC_RGUI,KC_RALT
+                                 KC_LALT,KC_LGUI,KC_LOWER,ESC_CTL,      BSP_SFT, KC_RAISE,KC_RGUI,KC_RALT
     ),
     [_LOWER] = LAYOUT_split(
-        _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_DQUO,  KC_QUOT, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
+        KANA, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_DQUO,  KC_QUOT, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
         KANA,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LCBR,  KC_RCBR, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
                  _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______,
                                    _______, _______, _______, _______,  KC_DEL,  _______, _______, _______
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_RAISE] = LAYOUT_split(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,   KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-        KANA,    KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,  _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______, _______,
+        _______,    KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,  _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______, _______,
                  KC_PSCR, KC_INS,  KC_SCRL, KC_PAUS, _______, _______,  _______, _______, _______, _______, _______, _______,
                                    _______, _______, _______, _______,  _______, _______, _______, _______
 
@@ -69,4 +69,51 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
         }
     }
     return false;
+}
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    static uint32_t key_timer;
+    static bool     lower_pressed    = false;
+    static bool     raise_pressed    = false;
+
+    switch (keycode) {
+        case KC_LOWER:
+            if (record->event.pressed) {
+                key_timer = timer_read32();
+                layer_on(_LOWER);
+                lower_pressed = true;
+            } else {
+                if (lower_pressed && timer_elapsed32(key_timer) <= TAPPING_TERM) {
+                    register_code(KC_SPC);
+                    unregister_code(KC_SPC);
+                }
+                layer_off(_LOWER);
+                lower_pressed = false;
+            }
+            return false;
+            break;
+        case KC_RAISE:
+            if (record->event.pressed) {
+                key_timer = timer_read32();
+                layer_on(_RAISE);
+                raise_pressed = true;
+            } else {
+                if (raise_pressed && timer_elapsed32(key_timer) <= TAPPING_TERM) {
+                    register_code(KC_ENT);
+                    unregister_code(KC_ENT);
+                }
+                layer_off(_RAISE);
+                raise_pressed = false;
+            }
+            return false;
+            break;
+        default:
+            if (record->event.pressed) {
+                lower_pressed    = false;
+                raise_pressed    = false;
+            }
+            break;
+    }
+    return true;
 }
